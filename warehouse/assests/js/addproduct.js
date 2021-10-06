875c43f254a83e5cf798ce77125bf500764a638b
akkhileshviswa <akkhilesh.viswa@aspiresys.com>
2021-10-06 13:31:05 +0530

    project

--- warehouse/assests/js/addproduct.js
+++ warehouse/assests/js/addproduct.js
@@ -1,6 +1,6 @@
 function add_row()
 {
-    $rowno = $("#product tr").length;
+    var $rowno = $("#product tr").length;
     $rowno = $rowno+1;
     $("#product tr:last").after("<tr id='row"+$rowno+"'><td class='right'><input type='text' name='name[]' placeholder='Enter Product Name'></td><td class='right'><input type='text' name='quantity[]' placeholder='Enter Quantity'></td><td class='right'><input type='text' name='code[]' placeholder='Enter Product Code'></td><td><input type='button' value='DELETE' id='remove' onclick=delete_row('row"+$rowno+"')></td></tr>");
 }
